4f6de60d6e20f520bf994c2472e63515ce4dd711
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-10-03 21:36:44 +1000

    Method to add a new Node to the end of a Singly Linked List

--- DSA/src/Linked_List/SinglyLinkedList.java
+++ DSA/src/Linked_List/SinglyLinkedList.java
@@ -12,4 +12,24 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+
+    //Method to insert a new node at the end of the list
+    public void insert(int value){
+        ListNode newNode = new ListNode(value);
+
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        //Traverse to the end of the list
+        ListNode current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+
+        //Insert new node at the end
+        current.next = newNode;
+    }
+
 }
