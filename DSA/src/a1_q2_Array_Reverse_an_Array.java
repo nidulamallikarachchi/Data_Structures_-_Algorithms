261fe5d9c318d7b8dd5eae601c9c015ff5bcac45
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-10-03 13:36:35 +1000

    Given an array, the task is to reverse the array

---
+++ DSA/src/a1_q2_Array_Reverse_an_Array.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class a1_q2_Array_Reverse_an_Array {
+
+    public static void main(String[] args) {
+        int[] originalArray = new int[]{2, 11, 5, 10, 7, 8};
+        int[] reversedArray = reverseArray(originalArray);;
+
+        System.out.println(Arrays.toString(reversedArray));
+    }
+
+    public static int[] reverseArray(int[] originalArray) {
+
+        int arrayLength = originalArray.length;
+
+        // Iterate only halfway through the array
+        for (int i = 0; i < arrayLength / 2; i++) {
+            // Swap elements from the front and back of the array
+            int temp = originalArray[i];
+            originalArray[i] = originalArray[arrayLength - 1 - i];
+            originalArray[arrayLength - 1 - i] = temp;
+        }
+
+        return originalArray;
+    }
+}
