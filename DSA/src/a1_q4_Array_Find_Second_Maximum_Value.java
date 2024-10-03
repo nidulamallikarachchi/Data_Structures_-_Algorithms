491db77f1921523a236dd007e24c0dd9093448ba
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-10-03 14:46:39 +1000

    Given an Array, Find the Second Maximum Value of the Array

---
+++ DSA/src/a1_q4_Array_Find_Second_Maximum_Value.java
@@ -0,0 +1,27 @@
+public class a1_q4_Array_Find_Second_Maximum_Value {
+    public static void main(String[] args) {
+        int[] array = {12,34,2,34,33,1};
+        int secondMax = findSecondMax(array);
+        System.out.println(secondMax);
+    }
+
+    public static int findSecondMax(int[] array){
+        int maxValue = Integer.MIN_VALUE;
+        int secondMaxValue = Integer.MIN_VALUE;
+
+        for(int num : array){
+            if(num>maxValue){
+                // If the new number found is greater than the Max Value, the Second Max value becomes the current max value
+                // And num becomes max value
+                secondMaxValue = maxValue;
+                maxValue = num;
+            } else if (num>secondMaxValue && num != maxValue) {
+                //If the new number is greater than secondMaxValue but is not equal to the maxValue
+                //Then the second max value is the new number found
+                secondMaxValue = num;
+            }
+        }
+        return secondMaxValue;
+
+    }
+}
