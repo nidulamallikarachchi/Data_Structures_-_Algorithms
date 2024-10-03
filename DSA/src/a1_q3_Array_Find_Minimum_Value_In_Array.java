038112ddf0e9701e2835f3bc2450d59d411c35c7
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-10-03 14:27:13 +1000

    Given an Array, Find the Minimum Value of the Array

---
+++ DSA/src/a1_q3_Array_Find_Minimum_Value_In_Array.java
@@ -0,0 +1,21 @@
+public class a1_q3_Array_Find_Minimum_Value_In_Array {
+
+    public static void main(String[] args) {
+        int[] array= {5,9,3,15,1,2};
+        int minValue = findMinimum(array);
+        System.out.println(minValue);
+    }
+
+    public static int findMinimum(int[] array){
+        // Initialize minValue with the first element
+        int minValue = array[0];
+
+        // Start from the second element, since the first is already set as minValue
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+}
